eb34397adb88e0fb788b7adfb3a78e611f165515
Everton da Rosa <everton3x@gmail.com>
2020-05-21 09:57:23 -0300

    Adicionado suporte para bal_desp.txt

---
+++ src/Transformer/FormataElementoTransformer.php
@@ -0,0 +1,23 @@
+<?php
+namespace CPAD\Transformer;
+
+use CPAD\Utils;
+
+/**
+ * formata ###### para #.#.##.##
+ *
+ * @author Everton
+ */
+class FormataElementoTransformer implements TransformerInterface
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function transform(string $data): string
+    {
+        return Utils::applyMask($data, '#.#.##.##');
+    }
+}
---
+++ trun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+clear
+
+rm -r test.csv
+
+php run.php
