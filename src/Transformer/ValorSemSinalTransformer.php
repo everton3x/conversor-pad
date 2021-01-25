9fc687b65ab615e157eaa9f62ec36fd5ffadb856
Everton da Rosa <everton3x@gmail.com>
2021-01-25 16:28:43 -0300

    Corrigida falha na conversao do bal_rec que dava burro com valores negativos menores que 1

--- src/Transformer/ValorSemSinalTransformer.php
+++ src/Transformer/ValorSemSinalTransformer.php
@@ -17,11 +17,14 @@ class ValorSemSinalTransformer implements TransformerInterface
     public function transform(string $data): string
     {
         $sinal = '';
-        if($data[0] === '-'){
-            $sinal = '-';
-        }
+        
+        
         $int = (int) substr($data, 0, strlen($data) - 2);
         $decimal = (string) substr($data, strlen($data) - 2, 2);
+
+        if($data[0] === '-' && $int === 0){
+            $sinal = '-';
+        }
 
         return "$sinal$int.$decimal";
     }
